ea12fa5167933c523f1633b3f889e88dbdbae3eb
lvidalc1 <lvidalc22@fpcoruna.afundacion.org>
2024-10-17 12:05:39 +0200

    commitEj6PHP

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -8,7 +8,11 @@ if (isset($_POST["funidad"])) {
 if ($_POST["funidad"] == "pulgada") {
 $v_pulgadas = $_POST["fcantidad"];
 $v_metros = $v_pulgadas * 0.0254;
-echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+echo $v_pulgadas." pulgada(s) = ".$v_metros." metro(s)";
+}elseif ($_POST["funidad"] == "pie") {
+$v_pies = $_POST["fcantidad"];
+$v_metros = $v_pies * 0.3048;
+echo $v_pies." pie(s) = ".$v_metros." metro(s)";
 } else {
 echo "Unidad no soportada";
 }
@@ -21,6 +25,8 @@ echo "Unidad no soportada";
 <input type="text" id="cantidad_input" name="fcantidad"><br>
 <input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 <label for="pulgada_input">Pulgada(s)</label><br>
+<input type="radio" id="pie_input" name="funidad" value="pie">
+<label for="pie_input">Pie(s)</label><br>
 <input type="radio" id="otro_input" name="funidad" value="otro">
 <label for="otro_input">Otro</label><br>
 <input type="submit" value="Convertir">
