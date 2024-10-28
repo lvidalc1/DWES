d4b302c00adf3d04d7e8f3985524e445cc2f2309
lvidalc1 <lvidalc22@fpcoruna.afundacion.org>
2024-10-28 10:03:47 +0100

    commitEj4EjsPHPySQL

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -18,13 +18,23 @@ echo '<img src="' . $only_row['url_imagen'] . '"/>'
 <h3>Comentarios:</h3>
 <ul>
 <?php
-$query2 = 'SELECT * FROM tComentarios WHERE id='.$id;
+$query2 = 'SELECT * FROM tComentarios WHERE libro_id='.$id;
 $result2 = mysqli_query($db, $query2) or die('Query error');
 while ($row = mysqli_fetch_array($result2)) {
 echo '<li>'.$row['comentario'].'</li>';
 }
-mysqli_close($db);
 ?>
 </ul>
+
+<p>Deja un nuevo comentario:</p>
+<form action="/comment.php" method="post">
+<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+<input type="hidden" name="id" value="<?php echo $id; ?>">
+<input type="submit" value="Comentar">
+</form>
+<?php
+mysqli_close($db);
+?>
+
 </body>
 </html>
