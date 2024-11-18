8f001a4a967be770d8ded6bcbc82b5d2d5fee787
lvidalc1 <lvidalc22@fpcoruna.afundacion.org>
2024-11-18 10:09:14 +0100

    commitOperacionesYCalcCorregidos

--- sprint3python/calculadora.py
+++ sprint3python/calculadora.py
@@ -1,41 +1,49 @@
-# Importar las funciones del archivo operaciones.py
+#importar las funciones del archivo operaciones.py
 from operaciones import suma, resta, multiplicacion, division
 
-REPETIR = "S"
-while REPETIR != "N":
-    print("-------------------------")
-    print("       Calculadora       ")
-    print("   Teclea la opción(1-4) ")
-    print("       1-Sumar           ")
-    print("       2-Restar          ")
-    print("       3-Multiplicar     ")
-    print("       4-Dividir         ")
-    print("-------------------------")
+repetir = "s"
+while repetir != "n":
+    print("-----------------------")
+    print("      Calculadora      ")
+    print("-----------------------")
+    print("      1-Sumar          ")
+    print("      2-Restar         ")
+    print("      3-Multiplicar    ")
+    print("      4-Dividir        ")
+    print("-----------------------")
    
-    # Solicitar la operación
-    OPCION = int(input("Operacion ? (1-4): "))
+    while True:
+        op = input("Operación a realizar? (1-4): ")
+    
+        if op.isdigit():#verificar si la opcion introducida es un número
+            op = int(op)
+            if op in [1, 2, 3, 4]:#verificar que está entre 1 y 4
+                break#si el valor es válido, salir de este ciclo
+            else:
+                print("El valor debe estar entre 1 y 4.")
+        else:
+            print("El valor debe ser un número válido entre 1 y 4.")
    
-    if OPCION not in range(1, 5):
-        print("Fuera de rango")
-        break
+    #solicitar los números
+    n1 = int(input("Introduce el primer número: "))
+    n2 = int(input("Introduce el segundo número: "))
+
    
-    # Solicitar los números
-    NUM1 = float(input("Introduce el primer número: "))
-    NUM2 = float(input("Introduce el segundo número: "))
-   
-    # Ejecutar la operación correspondiente e imprimir el resultado
-    if OPCION == 1:
-        resultado = suma(NUM1, NUM2)
+    #realizar la operación correspondiente e mostrar el resultado
+    if op == 1:
+        resultado = suma(n1, n2)
         print(f"Resultado: {resultado}")
-    elif OPCION == 2:
-        resultado = resta(NUM1, NUM2)
+    elif op == 2:
+        resultado = resta(n1, n2)
         print(f"Resultado: {resultado}")
-    elif OPCION == 3:
-        resultado = multiplicacion(NUM1, NUM2)
+    elif op == 3:
+        resultado = multiplicacion(n1, n2)
         print(f"Resultado: {resultado}")
-    elif OPCION == 4:
-        resultado = division(NUM1, NUM2)
+    elif op == 4:
+        resultado = division(n1, n2)
         print(f"Resultado: {resultado}")
        
-    # Preguntar si quiere realizar otra operación
-    REPETIR = input("¿Quieres realizar otra operación? (S/N): ").upper()
+    #preguntar si quiere realizar otra operación
+    repetir = input("¿Quiere realizar otra operación? (s/n): ").lower()
+    if(repetir!="s" and repetir!="n"):
+        print("No ha introducido ni s ni n")
